7be80bc0914ff141059f47800566e706d37e8b38
xialin <xzhang@quantcast.com>
2016-09-03 16:35:02 +0800

    Add Tracker and Game classes

---
+++ src/com/p2p/maze/Game.java
@@ -0,0 +1,12 @@
+package com.p2p.maze;
+
+/**
+ * Game
+ *
+ * Player in the maze game
+ * First player join the game will become primary server
+ * Second player join the game will become secondary server
+ * In the event of primary or secondary server crash, the system should promote a new pair of primary/secondary servers
+ */
+public class Game {
+}
